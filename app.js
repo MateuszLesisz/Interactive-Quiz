8f48a56b31dad06929c6a96cd637956b04875200
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-05-24 01:14:01 +0200

    add result on page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ["B", "B", "B", "B"];
 const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
 
 form.addEventListener("submit", e => {
 
@@ -8,7 +9,7 @@ e.preventDefault();
 let score = 0;
 const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-//check anwers
+//check answers
 
 userAnswers.forEach((answer, index) => {
 
@@ -17,6 +18,8 @@ userAnswers.forEach((answer, index) => {
     }
 });
 
-console.log(score);
+//show result on page
+result.querySelector("span").textContent = `${score}%`;
+result.classList.remove("d-none");
 
 });
