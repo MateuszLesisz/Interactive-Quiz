b3b175a0f20d9cb06b1fbfa0f3305b8bf7fb59cd
Mateusz Lesisz <m.lesisz97@wp.pl>
2022-05-24 01:31:25 +0200

    add Animation to score

--- app.js
+++ app.js
@@ -24,6 +24,16 @@ scrollTo(0,0);
 result.querySelector("span").textContent = `${score}%`;
 result.classList.remove("d-none");
 
+let output = 0;
+const timer = setInterval(() => {
+    result.querySelector("span").textContent = `${output}%`;
+    if(output === score) {
+        clearInterval(timer);
+    } else {
+        output++;
+    }
+}, 10 )
+
 });
 
 
